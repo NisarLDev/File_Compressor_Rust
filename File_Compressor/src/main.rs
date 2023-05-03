64b34b8e99dc228e48cbcd119a49cec854d533b9
NisarLDev <HarusBadah@proton.me>
2023-05-03 05:35:42 +0200

    Corrected main function and dependencies

--- File_Compressor/src/main.rs
+++ File_Compressor/src/main.rs
@@ -1,29 +1,29 @@
 extern crate flate2;
 
-
 use flate2::{write::GzEncoder, Compression};
-use std::env::args;
-use std::fs::File;
-use std::io::copy;
-use std::io::BufReader;
-use std::time::Instant;
+use std::{
+    env::args,
+    fs::File,
+    io::copy,
+    io::BufReader,
+    time::Instant,
+};
 
-
-fn main(){
-    if args().len() !=3 {
-	eprintln!("Usage: 'source' 'target'");
-	return;
+fn main() {
+    if args().len() != 3 {
+	    eprintln!("Usage: `source` `target`");
+	    return;
     }
-    Let mut input = BufReader::new(File::open(args().nth(1).unwrap()).unwrap());
-    Let output = File::create(args().nth(2).unwrap()).unwrap();
-    Let mut encoder = GzEncoder::new(output, Compression::default());
-    Let start = Instant::now();
+    let mut input = BufReader::new(File::open(args().nth(1).unwrap()).unwrap());
+    let output = File::create(args().nth(2).unwrap()).unwrap();
+    let mut encoder = GzEncoder::new(output, Compression::default());
+    let start = Instant::now();
     copy(&mut input, &mut encoder).unwrap();
-    Let output = encoder.finish().unrap();
+    let output = encoder.finish().unwrap();
     println!(
     	"Source len: {:?}",
-	input.get_ref().metadata().unwrap().len()
+	    input.get_ref().metadata().unwrap().len()
     );
-    println!("Target len:{:?}", output.metadata().unwrap().len());
+    println!("Target len: {:?}", output.metadata().unwrap().len());
     println!("Elapsed: {:?}", start.elapsed());
 }
